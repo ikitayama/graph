c969bccd0ad50b534361f844df02b7b4bd7adfb9
Daniel Pfeifer <daniel@pfeifer-mail.de>
2011-05-11 17:06:56 +0200

    latest from svn

--- module.cmake
+++
@@ -1,4 +0,0 @@
-ryppl_module(graph DEPENDS property_map tuple multi_index any random parameter regex)
-ryppl_module(graph_mpi DEPENDS mpi graph)
-
-# any is there because of the dependency on boost/property_map/dynamic_property_map.hpp
